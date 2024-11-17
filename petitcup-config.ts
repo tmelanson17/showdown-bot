d1d411176fbf37ad53c705340658f7572413dc69
tmelanson17 <tmelanson17@gmail.com>
2024-11-16 17:52:47 -0800

    Added petit cup

---
+++ petitcup-config.ts
@@ -0,0 +1,12 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+    {
+        name: "[Gen 1] Petit Cup",
+        mod: 'gen1stadium',
+        ruleset: ['Little Cup', 'Picked Team Size = 3', 'Min Level = 25', '!! Max Level = 30', 'Max Total Level = 80', 'Max Team Size = 6', 'Team Preview', 'Accuracy Moves Clause', 'Standard'],
+        banlist: ['Rhyhorn', 'Staryu', 'Grimer', 'Seel'],
+        unbanlist: ['Farfetch\'d', 'Ditto']
+    }
+];
